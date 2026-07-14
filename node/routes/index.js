3a6068fc44fd000bb30a23ecc7218779eee215b2
DajLx <durandaj027@gmail.com>
2026-07-14 07:56:35 -0400

    this is the first version of this project, is the begin.

---
+++ node/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router= express.Router();
+
+// router.use("/productos")
+// router.use("/categorias")
+// router.use("/proveedores")
+// router.use("/usuarios")
+
+module.exports= router;
---
+++ node/routes/proveedores.js
@@ -0,0 +1,7 @@
+const express= require("express")
+const router= express.Router();
+
+// aca iran todos los endpoints de proveedores.
+
+
+module.exports= router;
